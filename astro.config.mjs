b1cdba8c5da3987dc8bd52b5b498f6f5f9b17013
MvanSteenbergen <mvansteenbergen@proton.me>
2024-01-15 12:51:35 +0100

    changed config

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,6 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://mvansteenbergen.github.io',
+  base: 'src/pages/',
   integrations: [mdx(), sitemap(), tailwind()]
 });
